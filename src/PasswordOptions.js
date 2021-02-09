4b0192d99965147d120cecb76e2c2fa39c318496
MichaelRosado1 <mrosad4@illinois.edu>
2021-02-08 22:38:58 -0600

    added function to generate a password of length 8 that contains special characters, numbers, and lower/upper case letters

--- src/PasswordOptions.js
+++ src/PasswordOptions.js
@@ -3,24 +3,30 @@ import Options from './Options';
 const PasswordOptions = () => {
     const [passwordValue, setpasswordValue] = useState(''); 
     const [selectedProps, setSelectedProps] = useState([]);
-
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+-{}[]";
     const handleSelectedProps = (type) => {
         setSelectedProps([...selectedProps, type]);
     }
     const generatePassword = (e) => {
         e.preventDefault();
 
-        setpasswordValue(() => (
-            'randomPassword'
-        ))
+        setpasswordValue(() =>{
+            let password = "";
+            for (let i = 0; i < 8; i++) {
+                let charPosition = Math.random() * 79;
+                password = password + characters.charAt(charPosition);
+            }
+
+            return password;
+        }) 
     } 
     return (
         <div>
-            <form >
+            <form onSubmit={e => generatePassword(e)}>
                 <Options setSelected={handleSelectedProps}/>
-                <button type="submit" onSubmit={e => generatePassword(e)}>Generate a new password</button>
-                <button type="submit" onSubmit={e => setpasswordValue('')}>Clear password</button>
+                <button type="submit">Generate a new password</button>
             </form>
+            <button type="submit">Clear password</button>
             <label>
                 <input type="text" onChange={e => setpasswordValue(e.target.value)} value={passwordValue}></input>
             </label>
