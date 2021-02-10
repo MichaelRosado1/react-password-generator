b15526c022fd6780ca847358a13efa93d68a4e0b
MichaelRosado1 <mrosad4@illinois.edu>
2021-02-09 21:25:43 -0600

    added functionality that changes the selected chars based on a usestate passed to child

--- src/PasswordOptions.js
+++ src/PasswordOptions.js
@@ -3,19 +3,41 @@ import Options from './Options';
 const PasswordOptions = () => {
     const [passwordValue, setpasswordValue] = useState(''); 
     const [selectedProps, setSelectedProps] = useState([]);
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+-{}[]";
-    const handleSelectedProps = (type) => {
-        setSelectedProps([...selectedProps, type]);
+    const upperAndLowercharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const numbers = "123456789";
+    const specialChars = "!@#$%^&*()_-+={[}]";
+    
+    const getCharsToChooseFrom = () => {
+        let chars = '';
+        selectedProps.forEach((element) => {
+            if (element === 'number') {
+                chars += numbers;
+            }
+            if (element === 'specialChars') {
+                chars += specialChars;
+            }
+            if (element === 'upperlower') {
+                chars += upperAndLowercharacters;
+            }
+        });
+        return chars;
     }
     const generatePassword = (e) => {
         e.preventDefault();
-
+        setpasswordValue('');
+        setSelectedProps(['numbers', 'specialChars', 'upperlower']);
         setpasswordValue(() =>{
             let password = "";
-            for (let i = 0; i < 8; i++) {
-                let charPosition = Math.random() * 79;
-                password = password + characters.charAt(charPosition);
+            let charsToChooseFrom = getCharsToChooseFrom(); 
+            
+            for (let i = 0; i < charsToChooseFrom.length; i++) {
+                let charPosition = Math.random() * charsToChooseFrom.length;
+                password = password + charsToChooseFrom.charAt(charPosition);
             }
+            // for (let i = 0; i < 8; i++) {
+            //     let charPosition = Math.random() * 79;
+            //     password = password + characters.charAt(charPosition);
+            // }
 
             return password;
         }) 
@@ -23,7 +45,7 @@ const PasswordOptions = () => {
     return (
         <div>
             <form onSubmit={e => generatePassword(e)}>
-                <Options setSelected={handleSelectedProps}/>
+                <Options />
                 <button type="submit">Generate a new password</button>
             </form>
             <button type="submit">Clear password</button>
